ec958c9a55b788fcb42a7deaca90fc4104765c0d
Janne Mäki <janne.maki@evident.fi>
2018-01-04 15:33:31 +0200

    Style fixes

--- src/app/components/form/editableLabel.ts
+++ src/app/components/form/editableLabel.ts
@@ -15,7 +15,7 @@ mod.directive('editableLabel', () => {
     restrict: 'E',
     template: `<label>{{ctrl.title | translate}} 
                   <span ng-show="ctrl.infoText" class="fa fa-info-circle info" uib-tooltip="{{ctrl.infoText}}"></span>
-                  <span ng-show="ctrl.required && ctrl.isEditing()" class="fa fa-asterisk" style="color: red; font-size: small" uib-tooltip="{{'Required' | translate}}"></span>
+                  <span ng-show="ctrl.required && ctrl.isEditing()" class="fa fa-asterisk" uib-tooltip="{{'Required' | translate}}"></span>
                </label>`,
     bindToController: true,
     controllerAs: 'ctrl',
