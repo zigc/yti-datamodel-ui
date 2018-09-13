3f64fc0acf3146454a4f183209dbfff08e7f905c
Janne Mäki <janne.maki@evident.fi>
2018-09-13 14:59:15 +0300

    Fix source maps broken by ng-annotate processing

--- ng-annotate.plugin.ts
+++ ng-annotate.plugin.ts
@@ -1,14 +1,11 @@
+// inspired by https://github.com/jeffling/ng-annotate-webpack-plugin/
+
 const ngAnnotate = require('ng-annotate-patched');
 const SourceMapSource = require('webpack-core/lib/SourceMapSource');
 
 export class NgAnnotatePlugin {
 
   apply(compiler: any) {
-
-    const ngAnnotateOptions = {
-      add: true,
-      sourceMap: false
-    };
 
     compiler.hooks.compilation.tap('NgAnnotateWebpackPlugin', function(compilation: any) {
       compilation.hooks.optimizeChunkAssets.tapAsync('NgAnnotateWebpackPlugin', function(chunks: any, callback: any) {
@@ -17,15 +14,23 @@ export class NgAnnotatePlugin {
           for (const file of chunk.files) {
             if (file.endsWith('.js') && file !== 'vendor.js') {
 
-              const asset = compilation.assets[file];
-              const annotatedSource = ngAnnotate(asset.source(), ngAnnotateOptions);
+              const { source, map } = compilation.assets[file].sourceAndMap();
 
-              if (annotatedSource.errors) {
-                throw new Error(file + ': ' + annotatedSource.errors);
+              const annotated = ngAnnotate(source, {
+                add: true,
+                sourceMap: true,
+                map: {
+                  inFile: file,
+                  sourceRoot: ''
+                }
+              });
 
+              if (annotated.errors) {
+                throw new Error(file + ': ' + annotated.errors);
               }
 
-              compilation.assets[file] = new SourceMapSource(annotatedSource.src, file, asset.map());
+              compilation.assets[file] = map ? new SourceMapSource(annotated.src, file, JSON.parse(annotated.map), source, map)
+                                             : new SourceMapSource(annotated.src, file, map);
             }
           }
         }
