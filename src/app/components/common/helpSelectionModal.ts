cd8bc5fe7252da29fbdd01e844f7f90d0480b96f
JereVeijalainen <jereveijalainen82@gmail.com>
2018-10-09 17:15:41 +0300

    YTI-712 Add IDs to help selection modal

--- src/app/components/common/helpSelectionModal.ts
+++ src/app/components/common/helpSelectionModal.ts
@@ -1,6 +1,8 @@
 import { IModalService, IModalServiceInstance } from 'angular-ui-bootstrap';
 import { InteractiveHelp } from 'app/help/contract';
 import { InteractiveHelpDisplay } from 'app/help/components/interactiveHelpDisplay';
+import { labelNameToResourceIdIdentifier } from 'yti-common-ui/utils/resource';
+import { TranslateService } from '@ngx-translate/core';
 
 export class HelpSelectionModal {
 
@@ -15,7 +17,7 @@ export class HelpSelectionModal {
         
           <div class="modal-header">
             <h4 class="modal-title">
-              <a><i ng-click="$dismiss('cancel')" class="fas fa-times"></i></a>
+              <a><i id="cancel_help_selection_button" ng-click="$dismiss('cancel')" class="fas fa-times"></i></a>
               <span translate>Select help topic</span>
             </h4>
           </div>
@@ -23,7 +25,7 @@ export class HelpSelectionModal {
           <div class="modal-body full-height">
             <div class="content-box scrolling">
               <div class="search-results">            
-                <div class="search-result" ng-repeat="help in $ctrl.helps" ng-click="$ctrl.startHelp(help)">
+                <div id="{{$ctrl.getHelpLinkId(help.storyLine.title)}}" class="search-result" ng-repeat="help in $ctrl.helps" ng-click="$ctrl.startHelp(help)">
                   <div class="content" ng-class="{last: $last}">
                     <div class="title">{{help.storyLine.title | translate}}</div>
                     <div class="body">{{help.storyLine.description | translate}}</div>                  
@@ -34,7 +36,7 @@ export class HelpSelectionModal {
           </div>
          
           <div class="modal-footer">
-            <button class="btn btn-link" type="button" ng-click="$dismiss('cancel')" translate>Close</button>
+            <button id="close_help_selection_button" class="btn btn-link" type="button" ng-click="$dismiss('cancel')" translate>Close</button>
           </div>
         </div>
       `,
@@ -50,12 +52,19 @@ export class HelpSelectionModal {
 
 class HelpSelectionModalController {
 
-  constructor(private $uibModalInstance: IModalServiceInstance, public helps: InteractiveHelp[], private interactiveHelpDisplay: InteractiveHelpDisplay) {
+  constructor(private $uibModalInstance: IModalServiceInstance,
+              public helps: InteractiveHelp[],
+              private interactiveHelpDisplay: InteractiveHelpDisplay,
+              private translateService: TranslateService) {
     'ngInject';
   }
 
   startHelp(help: InteractiveHelp) {
     this.$uibModalInstance.close();
     this.interactiveHelpDisplay.open(help);
+  }
+
+  getHelpLinkId(title: string) {
+    return `${labelNameToResourceIdIdentifier(this.translateService.instant(title))}_help_link`;
   }
 }
