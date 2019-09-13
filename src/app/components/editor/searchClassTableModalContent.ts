620603ac408c9773ccda9f76594836c06cf1fbfd
JereVeijalainen <jereveijalainen82@gmail.com>
2019-09-13 08:52:25 +0300

    Fix indentations in the template of class table modal content

--- src/app/components/editor/searchClassTableModalContent.ts
+++ src/app/components/editor/searchClassTableModalContent.ts
@@ -13,98 +13,98 @@ import { ShowClassInfoModal } from './showClassInfoModal';
   selector: 'app-search-class-table-modal-content',
   template: `
       <table class="table table-sm" width="100%">
-          <thead>
-          <tr>
-              <th class="name-col">
-                  <sort-by-column-header [headerText]="'Name'"
-                                         [columnName]="'name'"
-                                         [model]="model"
-                                         [sortBy]="sortBy"
-                                         [filterExclude]="filterExclude"></sort-by-column-header>
-              </th>
-              <th class="model-col">
-                  <sort-by-column-header [headerText]="'Model'"
-                                         [columnName]="'model'"
-                                         [model]="model"
-                                         [sortBy]="sortBy"
-                                         [filterExclude]="filterExclude"></sort-by-column-header>
-              </th>
-              <th class="description-col">
-                  <sort-by-column-header [headerText]="'Description'"
-                                         [columnName]="'description'"
-                                         [model]="model"
-                                         [sortBy]="sortBy"
-                                         [filterExclude]="filterExclude"></sort-by-column-header>
-              </th>
-              <!-- Showing of super class is not implemented yet. -->
-              <!-- <th style="width: 20%" translate>Super class</th> -->
-              <th class="modified-at-col">
-                  <sort-by-column-header [headerText]="'Modified at'"
-                                         [columnName]="'modifiedAt'"
-                                         [model]="model"
-                                         [sortBy]="sortBy"
-                                         [filterExclude]="filterExclude"></sort-by-column-header>
-              </th>
-              <th class="menu-col"></th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr *ngFor="let searchResult of searchResults"
-              [id]="searchResultID(searchResult)"
-              [ngClass]="{'search-result': true, 'active': isSelected(searchResult)}"
-              (click)="itemSelected.emit(searchResult)"
-              [title]="itemTitle(searchResult)"
-              key-control-selection>
+        <thead>
+        <tr>
+          <th class="name-col">
+            <sort-by-column-header [headerText]="'Name'"
+                                   [columnName]="'name'"
+                                   [model]="model"
+                                   [sortBy]="sortBy"
+                                   [filterExclude]="filterExclude"></sort-by-column-header>
+          </th>
+          <th class="model-col">
+            <sort-by-column-header [headerText]="'Model'"
+                                   [columnName]="'model'"
+                                   [model]="model"
+                                   [sortBy]="sortBy"
+                                   [filterExclude]="filterExclude"></sort-by-column-header>
+          </th>
+          <th class="description-col">
+            <sort-by-column-header [headerText]="'Description'"
+                                   [columnName]="'description'"
+                                   [model]="model"
+                                   [sortBy]="sortBy"
+                                   [filterExclude]="filterExclude"></sort-by-column-header>
+          </th>
+          <!-- Showing of super class is not implemented yet. -->
+          <!-- <th style="width: 20%" translate>Super class</th> -->
+          <th class="modified-at-col">
+            <sort-by-column-header [headerText]="'Modified at'"
+                                   [columnName]="'modifiedAt'"
+                                   [model]="model"
+                                   [sortBy]="sortBy"
+                                   [filterExclude]="filterExclude"></sort-by-column-header>
+          </th>
+          <th class="menu-col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr *ngFor="let searchResult of searchResults"
+            [id]="searchResultID(searchResult)"
+            [ngClass]="{'search-result': true, 'active': isSelected(searchResult)}"
+            (click)="itemSelected.emit(searchResult)"
+            [title]="itemTitle(searchResult)"
+            key-control-selection>
 
-              <td class="name-col">
-                  <div>
-                      <app-ajax-loading-indicator-small *ngIf="isLoadingSelection(searchResult)"></app-ajax-loading-indicator-small>
-                      <highlight [text]="searchResult.label" [context]="model" [search]="searchText"></highlight>
-                  </div>
-                  <a [href]="model.linkToResource(searchResult.id)" target="_blank"
-                     [innerHTML]="searchResult.id.compact | highlight: searchText"></a>
-                  <div style="padding-top: 5px">
-                      <app-status [status]="searchResult.status"></app-status>
-                  </div>
-              </td>
-              <td class="model-col">
-                  <div>
-                      <highlight [text]="searchResult.definedBy.label" [context]="model" [search]="searchText"></highlight>
-                  </div>
-                  <div *ngIf="searchResult.definedBy.normalizedType">
-                      {{searchResult.definedBy.normalizedType | translate}}
-                  </div>
-                  <div>
-                      <span class="information-domains">
-                        <span class="badge badge-light" *ngFor="let infoDomain of searchResult.definedBy.classifications">
-                            {{showItemValue(infoDomain.label)}}
-                        </span>
-                      </span>
-                  </div>
-              </td>
-              <td class="description-col">
-                  <highlight [text]="searchResult.comment" [context]="model" [search]="searchText"></highlight>
-              </td>
-              <!-- Showing of super class is not implemented yet. -->
-              <!--
-              <td>
-                {{$ctrl.model.linkToResource(searchResult.superClassOf)}}
-              </td>
-              -->
-              <td class="modified-at-col">
-                  {{showItemValue(searchResult.modifiedAt)}}
-              </td>
-              <td class="menu-col">
-                <a [id]="classInfoLinkID(searchResult)"
-                    href="#"
-                    *ngIf="isSelected(searchResult)"
-                    (click)="showClassInfo()"
-                    [title]="('Show class information') | translate">
-                  <i class="fas fa-clone glyph-icon" aria-hidden="true"></i>
-                </a>
-              </td>
-          <tr>
-          </tbody>
+          <td class="name-col">
+            <div>
+              <app-ajax-loading-indicator-small *ngIf="isLoadingSelection(searchResult)"></app-ajax-loading-indicator-small>
+              <highlight [text]="searchResult.label" [context]="model" [search]="searchText"></highlight>
+            </div>
+            <a [href]="model.linkToResource(searchResult.id)" target="_blank"
+               [innerHTML]="searchResult.id.compact | highlight: searchText"></a>
+            <div style="padding-top: 5px">
+              <app-status [status]="searchResult.status"></app-status>
+            </div>
+          </td>
+          <td class="model-col">
+            <div>
+              <highlight [text]="searchResult.definedBy.label" [context]="model" [search]="searchText"></highlight>
+            </div>
+            <div *ngIf="searchResult.definedBy.normalizedType">
+              {{searchResult.definedBy.normalizedType | translate}}
+            </div>
+            <div>
+              <span class="information-domains">
+                <span class="badge badge-light" *ngFor="let infoDomain of searchResult.definedBy.classifications">
+                  {{showItemValue(infoDomain.label)}}
+                </span>
+              </span>
+            </div>
+          </td>
+          <td class="description-col">
+            <highlight [text]="searchResult.comment" [context]="model" [search]="searchText"></highlight>
+          </td>
+          <!-- Showing of super class is not implemented yet. -->
+          <!--
+          <td>
+            {{$ctrl.model.linkToResource(searchResult.superClassOf)}}
+          </td>
+          -->
+          <td class="modified-at-col">
+            {{showItemValue(searchResult.modifiedAt)}}
+          </td>
+          <td class="menu-col">
+            <a [id]="classInfoLinkID(searchResult)"
+               href="#"
+               *ngIf="isSelected(searchResult)"
+               (click)="showClassInfo()"
+               [title]="('Show class information') | translate">
+              <i class="fas fa-clone glyph-icon" aria-hidden="true"></i>
+            </a>
+          </td>
+        <tr>
+        </tbody>
       </table>
   `
 })
