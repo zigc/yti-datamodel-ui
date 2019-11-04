97459c863ca5e58b1d5ceac26d683f881bb4aa71
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-04 11:28:24 +0200

    YTI-1032: Fixing email subscription checkbox to change value after successful API response.

--- src/app/components/userdetails/user-details-subscriptions.component.ts
+++ src/app/components/userdetails/user-details-subscriptions.component.ts
@@ -146,8 +146,9 @@ export class UserDetailsSubscriptionsComponent implements OnInit {
     return this.subscriptionType !== 'DISABLED';
   }
 
-  toggleSubscription() {
+  toggleSubscription(event: Event) {
 
+    event.preventDefault();
     const subscriptionTargetType = this.subscriptionType === 'DAILY' ? 'DISABLED' : 'DAILY';
 
     this.openToggleNotifications(subscriptionTargetType === 'DAILY')
