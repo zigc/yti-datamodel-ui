4d3b9c857b0e9417430dd66e5bd4b7d10a8539b6
JereVeijalainen <jereveijalainen82@gmail.com>
2019-12-03 12:35:03 +0200

    Remove ConfigService from SubjectView because it is not in use there anymore

--- src/app/components/editor/subjectView.ts
+++ src/app/components/editor/subjectView.ts
@@ -3,8 +3,6 @@ import { Class } from '../../entities/class';
 import { Predicate } from '../../entities/predicate';
 import { Model } from '../../entities/model';
 import { LegacyComponent, modalCancelHandler } from '../../utils/angular';
-import { ConfigService } from '../../services/configService';
-import { Config } from '../../entities/config';
 import { EditableForm } from '../../components/form/editableEntityController';
 
 @LegacyComponent({
@@ -23,16 +21,11 @@ export class SubjectViewComponent {
 
   entity: Class|Predicate;
   model: Model;
-  config: Config;
   form: EditableForm;
   changeConceptDisabled: Boolean;
 
-  constructor(private searchConceptModal: SearchConceptModal,
-              private configService: ConfigService) {
+  constructor(private searchConceptModal: SearchConceptModal) {
     'ngInject';
-    this.configService.getConfig().then(urlConfig => {
-      this.config = urlConfig;
-    });
   }
 
   isEditing() {
